f4d9a6ef49a2fc2cda20ba7776f22017c697c0d2
Filip Dudarin <dudarin.it45.2020@uns.ac.rs>
2024-03-07 19:30:05 +0100

    Prvi zdt

--- OTS2023_ConventorApp/Form1.Designer.cs
+++ OTS2023_ConventorApp/Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rBtn2 = new System.Windows.Forms.RadioButton();
+            this.rBtn1 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.txtBox2);
+            this.panel1.Controls.Add(this.txtBox1);
+            this.panel1.Controls.Add(this.lbl2);
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(395, 423);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(210, 291);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "Convert";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(185, 204);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.ReadOnly = true;
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 4;
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(185, 165);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 3;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(7, 207);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(23, 13);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "lbl2";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(7, 168);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(23, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "lbl1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rBtn2);
+            this.groupBox1.Controls.Add(this.rBtn1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(129, 69);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Converter type";
+            // 
+            // rBtn2
+            // 
+            this.rBtn2.AutoSize = true;
+            this.rBtn2.Location = new System.Drawing.Point(7, 43);
+            this.rBtn2.Name = "rBtn2";
+            this.rBtn2.Size = new System.Drawing.Size(58, 17);
+            this.rBtn2.TabIndex = 1;
+            this.rBtn2.Text = "Length";
+            this.rBtn2.UseVisualStyleBackColor = true;
+            this.rBtn2.CheckedChanged += new System.EventHandler(this.rBtn2_CheckedChanged);
+            // 
+            // rBtn1
+            // 
+            this.rBtn1.AutoSize = true;
+            this.rBtn1.Checked = true;
+            this.rBtn1.Location = new System.Drawing.Point(7, 20);
+            this.rBtn1.Name = "rBtn1";
+            this.rBtn1.Size = new System.Drawing.Size(50, 17);
+            this.rBtn1.TabIndex = 0;
+            this.rBtn1.TabStop = true;
+            this.rBtn1.Text = "Mass";
+            this.rBtn1.UseVisualStyleBackColor = true;
+            this.rBtn1.CheckedChanged += new System.EventHandler(this.rBtn1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(419, 447);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.RadioButton rBtn2;
+        private System.Windows.Forms.RadioButton rBtn1;
+        private System.Windows.Forms.Button btn1;
     }
 }
 
--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -16,5 +16,36 @@ namespace OTS2023_ConventorApp
         {
             InitializeComponent();
         }
+
+        private void rBtn1_CheckedChanged(object sender, EventArgs e)
+        {
+            lbl1.Text = "Pounds";
+            lbl2.Text = "Kilograms";
+
+        }
+
+        private void rBtn2_CheckedChanged(object sender, EventArgs e)
+        {
+            lbl1.Text = "Feets";
+            lbl2.Text = "Meters";
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (rBtn1.Checked)
+            {
+                double a = Convert.ToInt32(txtBox1.Text);
+                double b = (double)(a * 0.45359237);
+                b = Math.Round(b, 3);
+                txtBox2.Text = b.ToString();
+            }
+            else {
+                double a = Convert.ToInt32(txtBox2.Text);
+                double b = (double)(a * 0.3048);
+                b = Math.Round(b, 3);
+                txtBox2.Text = b.ToString();
+            }
+        }
     }
 }
